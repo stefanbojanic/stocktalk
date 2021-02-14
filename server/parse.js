25f3c54e207f9f0a0d18b391f596a623831b04fa
george xu <george.xu99@hotmail.com>
2021-02-13 18:09:54 -0800

    Server.js: implement MVP for hot and raw endpoints

---
+++ server/parse.js
@@ -0,0 +1,21 @@
+const getTickers = (text) => {
+    const filtered = text.replace(/[^a-zA-Z ]/g, '')
+    const words = filtered.split(' ')
+    return words.filter(word => word.length <= 5 && verifyTicker(word))
+}
+
+
+// TODO: Check yahoo finance api? 
+const verifyTicker = (word) => {
+    const allowlist = {
+        "GME": true
+    };
+    return allowlist[word]
+}
+
+module.exports = {
+    getTickers
+}
+
+
+// 6043672733 - jerrys number
--- server/server.js
+++ server/server.js
@@ -1,10 +1,57 @@
-const express = require('express')
+require('dotenv').config()
+const express = require('express');
+const snoowrap = require('snoowrap');
+const constants = require('./constants');
+const {
+  getTickers
+} = require('./parse');
+
 const app = express()
 const port = 3000
 
+const r = new snoowrap({
+  userAgent: 'wsb-tickerbot',
+  clientId: 'sXFsYdT2GlAZfQ',
+  clientSecret: 'LA1E5JRSVevmUYPPqYPLfWlviGXu8w',
+  username: 'wework-street-bets',
+  password: 'PoonaniPrince69'
+});
+
+
+// setup 
+const SUBREDDIT = constants.WALLSTREETBETS
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+// app.get('/tickers', async (req, res) => {
+
+// })
+
+app.get('/raw', async (req, res) => {
+  const content = await r.getSubreddit(SUBREDDIT)
+    .getHot()
+  res.send(content)
+})
+
+app.get('/hot', async (req, res) => {
+  // gets hot posts on subreddit - chaining-tatum
+  const hotTitles = await r.getSubreddit(SUBREDDIT)
+    .getHot()
+    .map(post => ({
+      title: post.title,
+      content: getTickers(post.selftext),
+      flair: post.link_flair_richtext[0].t,
+      upvoteRatio: post.upvote_ratio,
+      upvotes: post.ups,
+    }))
+  // url, approved_at_utc, subreddit, selftext, aiuthor_fullname, saved, mod_reason_title, gilded, clicked, title,
+  // link_flair_richtext{ e:text, t:weekend discussion}, subredit_name_prefixed, link_flair_css_class, link_flair_text, 
+  // ric_flair_text_go_woo_on_a_bih
+
+  res.send(hotTitles)
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
