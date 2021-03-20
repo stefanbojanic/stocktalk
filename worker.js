1ec8df7a1b87fe2f7206694e39a6dea77b65e67e
unknown <ianmmah@gmail.com>
2021-03-20 12:28:35 -0700

    Clean up worker script

--- worker.js
+++ worker.js
@@ -1,11 +1,8 @@
-#! /app/.heroku/node/bin/node
-
 const { getHot } = require('./utils');
 
 const genhot = async () => {
     console.log("CRON: genhot")
     const counts = await getHot()
-    res.send(counts)
 }
 
 genhot()
