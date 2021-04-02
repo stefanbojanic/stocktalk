b7a54b6f306d9d113d764152e35b2319b1fcc75f
unknown <ianmmah@gmail.com>
2021-04-02 12:23:13 -0700

    Use created_utc for cursor

--- reddit.js
+++ reddit.js
@@ -15,44 +15,41 @@ const r = new snoowrap({
 
 const getDiscussionPosts = async () => {
     const cursorRef = db.collection('data').doc('ddCursor')
-    const cursor = (await cursorRef.get()).data()
-    console.log('Cursor:', cursor)
+    const ddCursor = (await cursorRef.get()).data()
+    console.log('Cursor:', ddCursor)
     const posts = await r.getHot(SUBREDDIT, { limit: 0 } )
     const discussions = []
     const pinned = []
     posts.forEach(post => {
         if(post.link_flair_richtext.find(f => f.t === "Daily Discussion") || post.link_flair_richtext.find(f => f.t === "Weekend Discussion")) {
             discussions.push(post)
-
-            // Cursor stuff could get messed if multiple pinned discussions
-            if (cursor.id === post.id) {
-                // This is the same post we've seen earlier today
-                cursor.offset = post.num_comments - cursor.numSeen
-                cursor.numSeen = post.num_comments
-            } else {
-                // We need to reset cursor, this is a new post
-                cursor.numSeen = post.num_comments
-                cursor.offset = 300 // Amount of comments to grab if we havent seen this post before
-                cursor.id = post.id
-            }
-
         } else if (post.stickied) {
             pinned.push(post)
         }
     })
-
-    await cursorRef.set(cursor)
-
     
     let comments = []
     await Promise.all(discussions.map(async dd => {
-        console.log(`Getting ${cursor.offset} comments from post ${cursor.id}`)
-        const comment = await dd.expandReplies({ limit: cursor.offset, depth: 1}).then(e => e)
+        // console.log(`Getting ${cursor.offset} comments from post ${cursor.id}`)
+        const post = await dd.expandReplies({ limit: 300, depth: 0 }).then(e => e)
 
-        console.log(`Got ${comment.comments.length} comments`)
+        console.log(`Got ${post.comments.length} comments`)
 
-        comments = [...comments, ...comment.comments]
+        post.comments.forEach(comment => {
+            if (comment.created_utc > ddCursor.cursor) {
+                comments.push(comment)
+            }
+        })
+
+        // comments = [...comments, ...post.comments]
     }))
+
+    console.log(`Kept ${comments.length} comments`)
+
+    if (comments.length) {
+        const cursor = comments[0].created_utc
+        cursorRef.set({ cursor })
+    }
     
     const counts = await contentsToCounts(comments.map(c => ({...c, tickerBody: c.body})))
     return counts
